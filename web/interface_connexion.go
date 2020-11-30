077d786ca98bdfe1cafa1bfaceddfc4e7c6369bb
e194188z <mateo.esteves@etu.univ-nantes.fr>
2020-11-30 11:36:37 +0100

    v5.6 Ajout de l'upload, du rename et du dossier de réception des fichiers uploads

--- web/interface_connexion.go
+++ web/interface_connexion.go
@@ -1,17 +1,14 @@
 package web
 
 import (
-	"bytes"
 	"fmt"
 	"gitlab.univ-nantes.fr/E192543L/projet-s3/BDD"
+	"io/ioutil"
+
 	//"github.com/gomodule/redigo/redis" pas sur de ce truc.
 	"html/template"
-	"io"
-	"io/ioutil"
 	"log"
-	"mime/multipart"
 	"net/http"
-	"os"
 )
 
 var etudiantCo BDD.Etudiant
@@ -25,6 +22,7 @@ func InitWeb() {
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
+	setupRoutes()
 }
 
 func pageEtudiant(w http.ResponseWriter, r *http.Request) {
@@ -40,25 +38,45 @@ func pageEtudiant(w http.ResponseWriter, r *http.Request) {
 		}
 	} else if r.Method == "POST" {
 		fmt.Printf("sdxlkhbfsdlkjgh")
-		if r.URL.String() == "/pageEtudiant?uploadFile" {
+		if r.URL.String() == "/pageEtudiant" {
 			// TODO récupération du fichier (qui se trouve dans r.FormValue["uploadfile"]
-			fmt.Println(r.FormValue("uploadfile"))
-			_ = r.ParseMultipartForm(32 << 20)
-			file, handler, err := r.FormFile("uploadfile")
+			fmt.Println("File Upload Endpoint Hit")
+
+			// Parse our multipart form, 10 << 20 specifies a maximum
+			// upload of 10 MB files.
+			r.ParseMultipartForm(10 << 20)
+			// FormFile returns the first file for the given key `myFile`
+			// it also returns the FileHeader so we can get the Filename,
+			// the Header and the size of the file
+			file, handler, err := r.FormFile("myFile")
 			if err != nil {
+				fmt.Println("Error Retrieving the File")
 				fmt.Println(err)
 				return
 			}
 			defer file.Close()
-			_, _ = fmt.Fprintf(w, "%v", handler.Header)
-			f, err := os.OpenFile("./test/"+handler.Filename, os.O_WRONLY|os.O_CREATE, 0666)
+			fmt.Printf("Uploaded File: %+v\n", handler.Filename)
+			fmt.Printf("File Size: %+v\n", handler.Size)
+			fmt.Printf("MIME Header: %+v\n", handler.Header)
+
+			// Create a temporary file within our directory that follows
+			// a particular naming pattern
+			tempFile, err := ioutil.TempFile("ressource/script_etudiants", "oh.txt")
 			if err != nil {
 				fmt.Println(err)
-				return
 			}
-			defer f.Close()
-			_, _ = io.Copy(f, file)
-			http.Redirect(w, r, "/pageEtudiant", http.StatusFound)
+			defer tempFile.Close()
+
+			// read all of the contents of our uploaded file into a
+			// byte array
+			fileBytes, err := ioutil.ReadAll(file)
+			if err != nil {
+				fmt.Println(err)
+			}
+			// write this byte array to our temporary file
+			tempFile.Write(fileBytes)
+			// return that we have successfully uploaded our file!
+			fmt.Fprintf(w, "Successfully Uploaded File\n")
 		}
 	}
 }
@@ -108,51 +126,7 @@ func accueil(w http.ResponseWriter, r *http.Request) {
 
 //
 
-func upload(filename string, targetUrl string) error {
-	bodyBuf := &bytes.Buffer{}
-	bodyWriter := multipart.NewWriter(bodyBuf)
-
-	// this step is very important
-	fileWriter, err := bodyWriter.CreateFormFile("uploadfile", filename)
-	if err != nil {
-		fmt.Println("error writing to buffer")
-		return err
-	}
-
-	// open file handle
-	fh, err := os.Open(filename)
-	if err != nil {
-		fmt.Println("error opening file")
-		return err
-	}
-	defer fh.Close()
-
-	//iocopy
-	_, err = io.Copy(fileWriter, fh)
-	if err != nil {
-		return err
-	}
-
-	contentType := bodyWriter.FormDataContentType()
-	_ = bodyWriter.Close()
-
-	resp, err := http.Post(targetUrl, contentType, bodyBuf)
-	if err != nil {
-		return err
-	}
-	defer resp.Body.Close()
-	resp_body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return err
-	}
-	fmt.Println(resp.Status)
-	fmt.Println(string(resp_body))
-	return nil
-}
-
-// sample usage
-func main() {
-	target_url := "http://localhost:8080/pageEtudiant?uploadFile"
-	filename := "./a.txt"
-	_ = upload(filename, target_url)
+func setupRoutes() {
+	http.HandleFunc("/pageEtudiant", pageEtudiant)
+	http.ListenAndServe(":8080", nil)
 }
