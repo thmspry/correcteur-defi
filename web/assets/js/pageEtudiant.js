01153a1040f9974c9cda334ef95db1a657f94b3b
Thomas PERAY <thomas.peray@etu.univ-nantes.fr>
2021-02-09 16:13:16 +0100

    0.10.8 : Suite des améliorations graphique pageEtudiant.html

--- web/assets/js/pageEtudiant.js
+++ web/assets/js/pageEtudiant.js
@@ -4,10 +4,27 @@ document.addEventListener('DOMContentLoaded', function() {
 
     let input = document.querySelector("input#input-file");
     let label = document.querySelector("#file label");
-    //let image = document.querySelector("#file .chose-file img");
+    let ligneCommande = document.querySelector("#ligne-commande");
+    const idEtu = document.querySelector("#id-etu").innerHTML;
+    const debutLigne = document.createElement("span");
+    debutLigne.setAttribute("class", "green-text");
+    debutLigne.innerHTML = idEtu + "@iut";
+    ligneCommande.append(debutLigne);
+    ligneCommande.append("$     ");
     input.addEventListener("change", function() {
-        label.innerHTML = label.innerHTML.replace("Choisir un fichier", input.value);
-        //image.remove();
+        let fileName = getFileName(input.value);
+        ligneCommande.append("./" + fileName);
+        label.innerHTML = label.innerHTML.replace("Choisir un fichier", fileName);
     });
 
 });
+
+function getFileName(filePath) {
+    let filePathSplit;
+    if (filePath.includes("/")) {   // Linux
+        filePathSplit = filePath.split("/");
+    } else {                                    // Windows
+        filePathSplit = filePath.split("\\");
+    }
+    return filePathSplit[filePathSplit.length-1];
+}
