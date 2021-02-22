cd9917edfc267aab0e91d69b0a677dfa82373ce8
Thomas PERAY <thomas.peray@etu.univ-nantes.fr>
2021-02-22 16:49:52 +0100

    0.11.13 : Problème d'onglet semi résolu

--- web/assets/js/pageAdmin.js
+++ web/assets/js/pageAdmin.js
@@ -1,3 +1,5 @@
+
+
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.datepicker');
     let optionsDatePicker = {
@@ -9,18 +11,31 @@ document.addEventListener('DOMContentLoaded', function() {
     var instances = M.Datepicker.init(elems, optionsDatePicker);
 
 
-
     changeInputFile("#file-defi");
     changeInputFile("#file-test");
 
+    var selectList = document.querySelectorAll('select');
+    var instancesSelect = M.FormSelect.init(selectList);
 
+    var instanceTab = M.Tabs.init(el, optionsTabs);
+    console.log(localStorage.getItem("current-tab"));
+    instanceTab.select(localStorage.getItem("current-tab"));
+
+});
+
+let tabLi = document.querySelectorAll(".tabs a")
+tabLi.forEach(li => {
+    li.addEventListener('click', () => {
+        let tab = document.querySelector(".active");
+        localStorage.setItem("current-tab", tab.getAttribute("href").substring(1));
+        console.log("stock " + tab.getAttribute("href").substring(1));
+    });
 });
 
 let el = document.querySelector(".tabs");
 let optionsTabs = {
     duration : 500
 }
-var instance = M.Tabs.init(el, optionsTabs);
 
 function getFileName(filePath) {
     let filePathSplit;
@@ -39,9 +54,4 @@ function changeInputFile(selector) {
         label.innerHTML = getFileName(input.value);
     });
 }
-
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('select');
-    var instances = M.FormSelect.init(elems);
-});
 
